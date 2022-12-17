39a177291f7a5753d799df37590de961e105fb1e
Md Sajjad Hossin <86626194+MdSajjadHossin@users.noreply.github.com>
2022-12-17 20:07:57 +0600

    codeforces 581A added

---
+++ 581A.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a , b;
+    cin>> a >> b;
+
+    if(a>b) cout<<b<< " ";
+    else cout<<a<<" ";
+
+    int result  = abs(a - b);
+
+    cout<<result/2;
+
+    return 0;
+}
